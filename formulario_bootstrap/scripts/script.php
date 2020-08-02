733560fd9ed5856ebd15d1672751c6c782c63add
Xaca <and1res@hotmail.com>
2020-08-02 09:51:54 -0500

    Termino ejercicio practico nuevo video

--- formulario_bootstrap/scripts/script.php
+++ formulario_bootstrap/scripts/script.php
@@ -1,7 +1,16 @@
 <?php
 	//var_dump($_POST);
 	$salt = "MATRIX";
-	$respuesta = array();
-	$respuesta["clave"]=md5($salt.$_POST["clave1"]);
-	echo json_encode($respuesta);
+	$usuario = array();
+	if(isset($_POST["nombre"]) && isset($_POST["apellido"])&& isset($_POST["celular"]) && isset($_POST["correo"]) && isset($_POST["ciudad"]))
+	{
+		$usuario["nombre"]  =   $_POST["nombre"];
+		$usuario["apellido"]=   $_POST["apellido"];
+		$usuario["celular"] =   $_POST["celular"];
+		$usuario["correo"]  =   $_POST["correo"];
+		$usuario["ciudad"]  =   $_POST["ciudad"];
+	}
+	$usuario["clave"] =   md5($salt.$_POST["clave1"]);
+
+	echo json_encode($usuario);
 ?>
