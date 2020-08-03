d92d062731e8b5d321cfeeea51a4ccc2fd98b1d6
Xaca <and1res@hotmail.com>
2020-08-02 19:49:13 -0500

    ajustes, ejercicio, pendiente organizar archivos

--- formulario_bootstrap/scripts/script.php
+++ formulario_bootstrap/scripts/script.php
@@ -1,4 +1,16 @@
 <?php
 	$salt = "MATRIX";
-	echo md5($slat.$_POST["clave1"]);
+	$usuario = array();
+	if(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["correo"]) && isset($_POST["celular"]) && isset($_POST["ciudad"])){
+	
+		$usuario["nombre"] = $_POST["nombre"];
+		$usuario["apellido"] = $_POST["apellido"];
+		$usuario["correo"] = $_POST["correo"];
+		$usuario["celular"] = $_POST["celular"];
+		$usuario["ciudad"] = $_POST["ciudad"];
+
+	}
+	$usuario["clave"] = md5($salt.$_POST["clave1"]);
+
+	echo json_encode($usuario);
 ?>
